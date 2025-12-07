5720c87b0521982238c8a9988568283c684834bb
Matas Marčiulionis <marciulionis.matas@gmail.com>
2025-12-07 19:02:02 +0200

    Disease CRUD

--- animal-backend-infrastructure/AnimalDbContext.cs
+++ animal-backend-infrastructure/AnimalDbContext.cs
@@ -6,4 +6,5 @@ namespace animal_backend_infrastructure;
 public class AnimalDbContext(DbContextOptions<AnimalDbContext> options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
+    public DbSet<Disease> Diseases { get; set; }
 }
