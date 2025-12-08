d0351fd1046ee1bb7fc0448296cb542be2ce7df5
jonvabalis <tmotiejus@gmail.com>
2025-12-08 18:43:31 +0200

    feat: add user entity

---
+++ animal-backend-domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using animal_backend_domain.Types;
+
+namespace animal_backend_domain.Entities;
+
+public class User : Entity
+{
+	public required string Name { get; set; }
+	public required string Surname { get; set; }
+	public required string Email { get; set; }
+	public required string Password { get; set; }
+	public required RoleType Role { get; set; }
+	public required string PhoneNumber { get; set; }
+	public required string PhotoUrl { get; set; }
+}
